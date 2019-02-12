0be408c526fb3461eacf5ff1ce65809e4eb78f38
AlexandreAnsel <alexandre.ansel.98@gmail.com>
2019-02-12 17:40:35 +0100

    Connexite
    donne la connexité

---
+++ connexite/Connexite.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "structure.h"
+#include <string.h>
+
+int connexite(Graph G, Sommet s):
+{
+    char file [25];
+    char sommet[2];
+    int positionIni=0;
+    int positionFin=0;
+
+    for ( int i = positionIni ; i <= nbrDeVoisin(sommet) ; i++)
+    {
+        strcpy(file[i], Voisin[i](Graph ,sommet);
+        positionFin++;
+    }
+
+    while (positionIni != positionFin)
+    {
+        for ( int i = positionIni ; i <= nbrDeVoisin(sommet) ; i++)
+        {
+            strcpy(file[i], Voisin[i](Graph ,sommet);
+            positionFin++;
+        }
+
+        positionIni++;
+
+
+
+
+        strcpy(sommet ,Voisin[i]);
+
