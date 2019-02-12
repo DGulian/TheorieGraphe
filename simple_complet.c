3a9da69c3971778e4a9455f1f2eea4d42f4d3638
root <root@localhost.localdomain>
2019-02-12 16:56:41 +0100

    Sur la branche master  Votre branche est à jour avec 'origin/master'.
     Modifications qui seront validées :
    	nouveau fichier : simple_complet.c

---
+++ simple_complet.c
@@ -0,0 +1,25 @@
+#include "structure.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+void * simple_complet(Graph graphe, char sommet){
+
+char * marque;
+char * fils;
+
+
+
+for (size_t i = 0; i < strlen(fils); i++) {
+  if(fils[i]!=marque[i]){
+   simple_complet(Graph graphe, char fils[i]);
+    }
+  }
+}
+
+
+int main(int argc, char const *argv[]) {
+
+
+  return 0;
+}
