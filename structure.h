458f8a469a54dcdd25169f36ddb1f20f3d9363f2
Papi <towc59@gmail.com>
2019-02-12 15:56:21 +0100

    Changes to be committed: 	modified:   structure.h

--- structure.h
+++ structure.h
@@ -6,10 +6,4 @@ typedef struct{
     int nb_liens;
 }Graph;
 
-
-<<<<<<< HEAD
 char * nbrVoisin(Graph graphe, char sommet, int *nbrVois);
-=======
-char * nbrVoisin(Graph graphe, char sommet);
-
->>>>>>> 9f2ccc3da909bc87cedc237cba6201c4f3c35a03
